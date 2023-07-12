8b93abccfae8c9ffb6b4dcf37094ac5899e6db18
Swathy Krishna <138104838+swathykrishna95@users.noreply.github.com>
2023-07-12 12:17:46 +0530

    Add a function computerChoice for returning computer's hand

---
+++ script.js
@@ -0,0 +1,9 @@
+let getComputerChoice = () => {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let compChoice = choices[Math.floor(Math.random() * 3)]
+    console.log(compChoice);
+}
+
+let playRound = () => {
+    let playerSelection = prompt("Enter your choice: (rock, paper or scissors:" );
+}
