313410954061ccc995f4d96d5b5889c4e9948a98
Swathy Krishna <138104838+swathykrishna95@users.noreply.github.com>
2023-07-12 18:54:46 +0530

    Add a loop inside game to play it 5 times

--- script.js
+++ script.js
@@ -6,25 +6,30 @@ let getComputerChoice = () => {
 
 let playRound = (playerSelection, computerSelection) => {
     
-        if(playerSelection .toLowerCase() === computerSelection){
-            console.log(`Both hands are ${playerSelection}. Its a draw!`);
-        }
-        else if(playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock'){
-            console.log(`${playerSelection} beats ${computerSelection}. You win!`);
-        }
-        
-        else{
-            console.log(`${computerSelection} beats ${playerSelection}. You lose!`)
-        }
+    if(playerSelection .toLowerCase() === computerSelection){
+        return(`Both hands are ${playerSelection}. Its a draw!`);
+    }
+    else if(playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock'){
+        return (`${playerSelection} beats ${computerSelection}. You win!`);
+    }
+    
+    else{
+        return (`${computerSelection} beats ${playerSelection}. You lose!`)
+    }
 }
 
 let game = () => {
+    let i = 5;
+    while(i > 0)
+    {
     let playerSelection = prompt("Enter your choice: (rock, paper or scissors:" ).toLowerCase();
     if(playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors'){
         let computerSelection = getComputerChoice().toLowerCase();
-        playRound(playerSelection, computerSelection);
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
     }
     else{
         console.log("Please select a valid hand.")
+    }
     }
 }
