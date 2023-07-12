725d38c39cb4ca6672a1c83395427827fe9041df
Swathy Krishna <138104838+swathykrishna95@users.noreply.github.com>
2023-07-12 18:39:00 +0530

    Finish the logic for the game and remind me to commit more

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ let getComputerChoice = () => {
     let choices = ['Rock', 'Paper', 'Scissors'];
     let compChoice = choices[Math.floor(Math.random() * 3)]
     console.log(compChoice);
+    return compChoice;
 }
 
 let playRound = () => {
     let playerSelection = prompt("Enter your choice: (rock, paper or scissors:" );
+    if(playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors'){
+        let computerSelection = getComputerChoice().toLowerCase();
+        if(playerSelection .toLowerCase() === computerSelection){
+            console.log(`Both hands are ${playerSelection}. Its a draw!`);
+        }
+        else if(playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock'){
+            console.log(`${playerSelection} beats ${computerSelection}. You win!`);
+        }
+        
+        else{
+            console.log(`${computerSelection} beats ${playerSelection}. You lose!`)
+        }
+    }
+    else{
+        console.log("Please select a valid hand.")
+    }
 }
