aa9fac89dbb4b5a254c3f33117279b9b143d7d6a
Swathy Krishna <138104838+swathykrishna95@users.noreply.github.com>
2023-07-12 19:14:17 +0530

    Add a score variable and win flags which keeps track of who wins and display a result at the end

--- script.js
+++ script.js
@@ -5,31 +5,54 @@ let getComputerChoice = () => {
 }
 
 let playRound = (playerSelection, computerSelection) => {
-    
+    let win = "";
     if(playerSelection .toLowerCase() === computerSelection){
-        return(`Both hands are ${playerSelection}. Its a draw!`);
+        console.log(`Both hands are ${playerSelection}. Its a draw!`);
     }
     else if(playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock'){
-        return (`${playerSelection} beats ${computerSelection}. You win!`);
+        console.log(`${playerSelection} beats ${computerSelection}. You win!`);
+        win = "player win"
     }
     
     else{
-        return (`${computerSelection} beats ${playerSelection}. You lose!`)
+        console.log(`${computerSelection} beats ${playerSelection}. You lose!`);
+        win = "computer win";
     }
+    return win;
 }
 
 let game = () => {
     let i = 5;
+    let score = 0;
+    let compScore = 0;
+    let result = "";
     while(i > 0)
     {
     let playerSelection = prompt("Enter your choice: (rock, paper or scissors:" ).toLowerCase();
     if(playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors'){
         let computerSelection = getComputerChoice().toLowerCase();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
+        result = playRound(playerSelection, computerSelection);
     }
     else{
         console.log("Please select a valid hand.")
     }
+    i-= 1;
+    if(result == 'player win'){
+        score += 1;
+    }
+    else{
+        compScore += 1;
+    }
+    }
+    console.log(score, compScore);
+    if(score == compScore)
+    {
+        console.log(`Both scored ${score}. Match is a draw!`)
+    }
+    else if(score > compScore){
+        console.log(`Player scored ${score} and computer scored ${compScore}. Player wins!`)
+    }
+    else{
+        console.log(`Player scored ${score} and computer scored ${compScore}. Player loses!`)
     }
 }
