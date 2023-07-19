d43c663d30787a03405562e1b07834d27c413953
Swathy Krishna <138104838+swathykrishna95@users.noreply.github.com>
2023-07-19 18:22:20 +0530

    Add css styles to buttons, page and scoreboard

--- script.js
+++ script.js
@@ -21,49 +21,58 @@ let playRound = (playerSelection, computerSelection) => {
     return win;
 }
 
+let buttonDisabler = () => {
+    buttons.forEach(button => {
+        button.disabled = true;
+    })
+}
+
 let game = (playerSelection) => {
     if(playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors'){
         let computerSelection = getComputerChoice().toLowerCase();
         result = playRound(playerSelection, computerSelection);
     }
     
-    //i-= 1;
+  
     if(result == 'player win'){
         score += 1;
-        console.log(score)
     }
     else if(result == 'computer win') {
         compScore += 1;
-        console.log(compScore)
     }
-    console.log(score,compScore)
+    scoreDiv.textContent = `Your score : ${score}, Computer's score : ${compScore}`;
     if(score == 5 || compScore == 5){
-        console.log(score, compScore);
+        scoreDiv.textContent = ` Final scores! Your score : ${score}, Computer's score : ${compScore}`;
         if(score == compScore)
         {
             resultDiv.textContent = `Both scored ${score}. Match is a draw!`;
+            buttonDisabler();
         }
         else if(score > compScore){
             resultDiv.textContent =  `Player scored ${score} and computer scored ${compScore}. Player wins!`;
+            buttonDisabler();
         }
         else{
             resultDiv.textContent = `Player scored ${score} and computer scored ${compScore}. Player loses!`;
+            buttonDisabler();
         }
     }
+  
     }
   
-//let game = (playerChoice) => {
-    //let i = 5;
+
 const buttons = document.querySelectorAll('button');
 const resultDiv = document.querySelector('#result');
+const scoreDiv = document.querySelector('#score');
+
 let score = 0;
 let compScore = 0;
 let result = "";
 
+
+
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-    //console.log(button.textContent);
-    //game(button.textContent.toLowerCase());
     resultDiv.textContent = "The game is afoot!";
     playerSelection = button.textContent.toLowerCase();
     game(playerSelection);
@@ -71,10 +80,8 @@ buttons.forEach((button) => {
 })
 });
 
+
    
-//}
-//console.log(playerSelection)
-    
 
 
 
